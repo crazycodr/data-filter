22a8e4558b6b3be052da69a40bf1d902abd2d13d
crazyone-crazycoders <crazyone@crazycoders.net>
2013-07-27 07:29:10 -0400

    Final source code for 2.0 dev

--- src/CrazyCodr/Data/Filter/FilterNotFoundException.php
+++ src/CrazyCodr/Data/Filter/FilterNotFoundException.php
@@ -1,30 +1,30 @@
-<?php
-namespace CrazyCodr\Data\Filter;
-
-/**
-* This exception is raised when an operation tries to access a filter that doesn't exist in a filter container
-*
-* @uses     OutOfRangeException
-*
-* @category Exceptions
-* @package  CrazyCodr/Data-Filter
-* @author   CrazyOne@CrazyCoders
-* @license  MIT
-* @link     crazycoders.net  
-*/
-class FilterNotFoundException extends \OutOfRangeException
-{
-
-    /**
-     * Builds a new exception
-     * 
-     * @param mixed $name Name of the intended filter that failed
-     *
-     * @access public
-     */
-	public function __construct($name)
-	{
-		parent::__construct('Filter named "'.htmlentities($name).'" not found in current filter container');
-	}
-
+<?php
+namespace CrazyCodr\Data\Filter;
+
+/**
+* This exception is raised when an operation tries to access a filter that doesn't exist in a filter container
+*
+* @uses     \OutOfRangeException
+*
+* @category Exceptions
+* @package  CrazyCodr/Data-Filter
+* @author   CrazyOne@CrazyCoders
+* @license  MIT
+* @link     crazycoders.net  
+*/
+class FilterNotFoundException extends \OutOfRangeException
+{
+
+    /**
+     * Builds a new exception
+     * 
+     * @param mixed $name Name of the intended filter that failed
+     *
+     * @access public
+     */
+	public function __construct($name)
+	{
+		parent::__construct('Filter named "'.htmlentities($name).'" not found in current filter container');
+	}
+
 }
